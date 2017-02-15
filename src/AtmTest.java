14d9fed1f1e6f8884fef9103ee47132fee490b1c
John Krause <jekrause@uwm.edu>
2017-02-15 14:24:20 -0600

    ATM test2

--- src/AtmTest.java
+++ src/AtmTest.java
@@ -1,48 +1,55 @@
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class AtmTest {
+	static Atm atm;
+	ByteArrayInputStream in;
 	
 	@BeforeClass
-	public void setup()
+	public static void setup()
 	{
-		Atm atm = new Atm();
-		Card card1 = new Card(1234);
-		Card card2 = new Card(6789);
+		atm = new Atm();
+		
 	}
 
 	//Successfully validate	and	withdrawal	of	$20	from account 1234
 	@Test
 	public void testA() {
-		assertTrue(atm.validate(card1));
-		atm.withdraw(20);
-		atm.exit();
+		atm.start();
+		//enter account
+		in = new ByteArrayInputStream("6789".getBytes());
+		System.setIn(in);
+		//enter pin
+		
+		
 	}
 	
 	//Successfully validate	and	withdrawal	of	$80	from account 1234
-	@Test
-	public void testB() {
-		assertTrue(atm.validate(card1));
-		atm.withdraw(80);
-		atm.exit();
-	}
-	
-	//Incorrect validation of account 6789
-	@Test
-	public void testC() {
-		
-	}
-	
-	//Successfully deposit $20 into account 6789
-	@Test
-	public void testD() {
-		assertTrue(atm.validate(card2));
-		atm.deposit(20);
-		atm.exit();
-		
-	}
+//	@Test
+//	public void testB() {
+//		assertTrue(atm.validate(card1));
+//		atm.withdraw(80);
+//		atm.exit();
+//	}
+//	
+//	//Incorrect validation of account 6789
+//	@Test
+//	public void testC() {
+//		
+//	}
+//	
+//	//Successfully deposit $20 into account 6789
+//	@Test
+//	public void testD() {
+//		assertTrue(atm.validate(card2));
+//		atm.deposit(20);
+//		atm.exit();
+//		
+//	}
 
 }
