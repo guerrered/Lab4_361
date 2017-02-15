4f151a299125998ba11033f274ac9799a618165d
guerrered <guerrered8@gmail.com>
2017-02-15 13:59:23 -0600

    atm

--- src/Atm.java
+++ src/Atm.java
@@ -1,9 +1,62 @@
 import java.util.Scanner;
 
 public class Atm {
+	Bank bank;
+	Scanner scan;
+
+	public Atm(){
+		bank = new Bank();
+		scan = new Scanner(System.in);
+	}
 	
-	public static void main(String [] args){
-		int userIn;
+	public void start(){
+		while(scan.hasNext()== true){
+			int accountNum = scan.nextInt();
+			boolean valid = bank.validate(accountNum);
+			String choice = courseofAction();
+			if(choice == "W"){
+				Withdraw(accountNum);
+			}
+			else if(choice == "D"){
+				Deposit(accountNum);
+			}
+		}
+	}
+	
+	public boolean validate(int accountNum){
+		int PIN = enterPin();
+		return bank.validate(accountNum, PIN);
+	}
+	
+	public int enterPin(){
+		int PIN = scan.nextInt();
+		return PIN;
+	}
+	
+	public String courseofAction(){
+		System.out.println("To deposit enter 'D' to withdraw enter 'W'\n");
+		return scan.next();
+	}
+	
+	public void Withdraw(int accountNum){
+		System.out.println("How much would you like to Withdraw?\n");
+		double amount = scan.nextDouble();
+		bank.withdraw(amount);
+		printReceipt("Deposit", accountNum, amount);
+	}
+	public void Deposit(int accountNum){
+		System.out.println("How much would you like to Deposit?\n");
+		double amount = scan.nextDouble();
+		bank.deposit(amount);
+		printReceipt("Deposit", accountNum, amount);
+	}
+	
+	public void printReceipt(String action, int accountNum, double amount){
+		System.out.println(action + "from " + accountNum +" : " + amount);
+	}
+}
+/*
+ * String userIn;
 		Scanner stdIn = new Scanner(System.in);
 		System.out.println("ATM is ready for use!");
 		System.out.println("---------------------");
@@ -13,12 +66,4 @@ public class Atm {
 			System.out.println("Account not found. Exiting");
 			System.exit(0);
 		}
-		else
-		{
-			System.out.println("Plase enter pin number");
-		}
-		
-		
-		
-	}
-}
+ */
