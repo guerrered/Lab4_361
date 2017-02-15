be880e6df7943aec3e215d2b01a43ac45fa652b1
KRAUSE1 <krausedev@gmail.com>
2017-02-14 19:01:32 -0600

    some fields and method headings added
    Additionally, I switched "Account" to be an inner class of "Bank"

--- src/Atm.java
+++ src/Atm.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
 
 public class Atm {
-
+	
+	public static void main(String [] args){
+		String userIn;
+		Scanner stdIn = new Scanner(System.in);
+		System.out.println("ATM is ready for use!");
+		System.out.println("---------------------");
+		System.out.println("Please enter your account number!");
+		userIn = stdIn.next();
+		if(!Bank.validate(userIn)){
+			System.out.println("Account not found. Exiting");
+			System.exit(0);
+		}
+		
+	}
 }
--- src/Bank.java
+++ src/Bank.java
@@ -1,8 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bank {
+	static List<Account> bankAcc = new ArrayList<Account>();
+	private Account curAccount;
+	
+	public class Account{
+		private int	accountNumber;
+		private int pin;
+		private double balance;
 
+		public Account(int accountNumber, int pin){
+			this.pin = pin;
+			this.accountNumber = accountNumber;
+		}
+	}
 	
 	
 	
+	public boolean deposit(){
+		return false;
+	}
 	
+	public boolean withdraw(){
+		return false;
+	}
+	
+	public String getBalance(){
+		if(curAccount!=null){
+			return "$" + curAccount.balance;
+		}
+		return null;
+	}
+
+	public static boolean validate(String userIn) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
 }
