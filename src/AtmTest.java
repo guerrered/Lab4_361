fde7d5f7cab8ae447082eba1c5e87c28807f71ea
Unknown <guerrered8@gmail.com>
2017-02-17 14:01:37 -0600

    cards added

--- src/AtmTest.java
+++ src/AtmTest.java
@@ -6,24 +6,26 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-//import Bank.Account;
 
 public class AtmTest {
 	static Atm atm;
 	ByteArrayInputStream in;
+	Card c1, c2;
 	
 	@Before
 	public void setup()
 	{
 		atm = new Atm();
 		atm.bank.addAccount(1234,6789,80.0);
+		c1 = new Card(1234);
 		atm.bank.addAccount(6789,4321,60.0);
+		c2 = new Card(6789);
 	}
 
 	//Successfully validate	and	withdrawal	of	$20	from account 1234
 	@Test
 	public void testA() {
-		assertEquals(true,atm.validate(1234,6789));
+		assertEquals(true,atm.validate(c1.getAccountNum(),6789));
 		atm.Withdraw(20);
 		assertEquals("$60.0",atm.getBalance());
 		System.out.println(atm.getBalance());
@@ -32,7 +34,7 @@ public class AtmTest {
 	
 	@Test
 	public void testB(){
-		assertEquals(true,atm.validate(1234,6789));
+		assertEquals(true,atm.validate(c1.getAccountNum(),6789));
 		atm.Withdraw(80);
 		assertEquals("$0.0",atm.getBalance());
 		System.out.println(atm.getBalance());
@@ -43,52 +45,17 @@ public class AtmTest {
 	@Test
 	public void testC()
 	{
-		assertEquals(false,atm.validate(6789,1234));
+		assertEquals(false,atm.validate(c2.getAccountNum(),1234));
 		
 	}
 	
 	@Test
 	public void testD()
 	{
-		assertEquals(true,atm.validate(6789,4321));
+		assertEquals(true,atm.validate(c2.getAccountNum(),4321));
 		atm.Deposit(20);
 		assertEquals("$80.0",atm.getBalance());
 		System.out.println(atm.getBalance());
 		
 	}
-		/*//enter account
-		in = new ByteArrayInputStream("6789".getBytes());
-		System.setIn(in);
-		atm.start();
-		//enter pin
-		
-		
-	}
-	
-	
-	
-	
-	//Successfully validate	and	withdrawal	of	$80	from account 1234
-//	@Test
-//	public void testB() {
-//		assertTrue(atm.validate(card1));
-//		atm.withdraw(80);
-//		atm.exit();
-//	}
-//	
-//	//Incorrect validation of account 6789
-//	@Test
-//	public void testC() {
-//		
-//	}
-//	
-//	//Successfully deposit $20 into account 6789
-//	@Test
-//	public void testD() {
-//		assertTrue(atm.validate(card2));
-//		atm.deposit(20);
-//		atm.exit();
-//		
-//	}
-*/
 }
