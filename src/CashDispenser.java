f678626e89885b795b385089f8b40d2dbab28b76
dishenwen <dwen@uwm.edu>
2017-02-21 22:11:04 -0600

    Cash dispenser

--- src/CashDispenser.java
+++ src/CashDispenser.java
@@ -2,11 +2,20 @@
 public class CashDispenser {
 static double TotalAmountInATM =10000.0;
 	
-public void CashDispen(double amount)
+public boolean CashDispen(double amount)
 {
-	
+	if(amount>TotalAmountInATM)
+	{
+      System.out.println("No enough cash for dispen, transaction cancel");
+		return false;
+	}
+	else
+	{
 	TotalAmountInATM-=amount;
+	}
+	return true;
 }
+
 
 public void CashDeposit(double amount)
 {
