d3b29e1a4f1d1d64fe3f84a140278409c2de0973
dishenwen <dwen@uwm.edu>
2017-02-22 14:30:58 -0600

    Similator

--- src/Simulator.java
+++ src/Simulator.java
@@ -1,6 +1,4 @@
-import java.awt.List;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,12 +7,14 @@ import java.util.Scanner;
 public class Simulator {
 
 	
-	
+
  ArrayList<String> list =new ArrayList<String>();
-	
+    Atm atm;	
+    Card c1;
+    double num;
 	public Simulator(){
 	
-		    Atm atm=new Atm();
+		    atm=new Atm();
 			Scanner scan = new Scanner(System.in);
 			System.out.println("Enter 'C' for console input or 'F' for file input\n");
 			String choice = scan.nextLine();
@@ -40,7 +40,7 @@ public class Simulator {
 	try(BufferedReader buff = new BufferedReader(new FileReader(file))){
 		String currentLine;
 		while((currentLine = buff.readLine()) != null){
-			commandExec(currentLine);
+			commandExec(currentLine,null);
 		}
 	}catch(IOException e){
 		e.printStackTrace();
@@ -54,30 +54,55 @@ public class Simulator {
 		System.out.println("Enter Command\n");
 		while(scan.hasNext()){
 			command = scan.nextLine();
-			commandExec(command);
+			commandExec(command,scan);
 			System.out.println("Enter Command\n");
 		}
 	}
 	
-	public void commandExec(String command){
+	public void commandExec(String command, Scanner scan){
 		System.out.println(command);
 		String [] instructions = command.split(" ");
 		switch(instructions[0]){
 		case("CARDREAD"):
-			
-			
+		c1 = new Card(Integer.parseInt(instructions[1]));
 			break;
 		case("NUM"):
-			
+			num=Double.parseDouble(instructions[1]);
 			break;
-		case("DIS"):
-			
-			break;
-	
+		case("BUTTON"):
+			commandBal(instructions[1],scan);
+	        break;
 		default:
 			System.out.println("Not a valid command\n");
 			break;
 		}
+	}
+	
+	public void commandBal(String str,Scanner scan)
+	{
+		switch(str)
+		{
+		case("W"):
+			//Dislpay();
+		System.out.println("How much you want to withdraw?");
+		 double cnt=scan.nextDouble();
+		 atm.Withdraw(cnt);		
+			break;
+		case("CB"):
+			//Dislpay()
+			atm.getBalance();
+			break;
+		case("CANCEL"):
+		//Dislpay()
+			
+			
+			break;
+		
+		}
+		
+		
+		
+		
 	}
 	
  }
